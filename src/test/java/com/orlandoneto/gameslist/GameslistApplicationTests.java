e10e905644774ac1af853d054a0088a1a87ff128
Orlando Neto <neto020735@gmail.com>
2024-11-15 18:40:21 -0300

    Inicio do Projeto

---
+++ src/test/java/com/orlandoneto/gameslist/GameslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlandoneto.gameslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
